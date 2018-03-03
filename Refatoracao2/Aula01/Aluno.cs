0b1c33dfdc7ec0f4108f460fbfdf841a6c304fb8
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-03-03 13:54:35 -0300

    Emcapsular coleção

---
+++ Refatoracao2/Aula01/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refatoracao2.Aula01
+{
+    public class Aluno
+    {
+        private IList<string> _cursos;
+
+        public IReadOnlyCollection<string> Cursos
+        {
+            get => new ReadOnlyCollection<string>(_cursos);
+        }
+    }
+}
