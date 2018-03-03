d29e573ae6fe1a0d9964f78bb140728e77ff41b3
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-03-03 14:02:54 -0300

    Adicionando métodos para adicionar e remover itens da coleção

--- Refatoracao2/Aula01/Aluno.cs
+++ Refatoracao2/Aula01/Aluno.cs
@@ -20,5 +20,32 @@ namespace Refatoracao2.Aula01
         {
             _cursos = new List<string>();
         }
+
+        public void AdicionaCurso(string curso)
+        {
+            if (string.IsNullOrEmpty(curso))
+            {
+                throw new ArgumentException("O nome do curso não pode ser nulo ou vazio");
+            }
+            else
+            {
+                _cursos.Add(curso);
+            }
+        }
+
+        public void RemoveCurso(string curso)
+        {
+            if (string.IsNullOrEmpty(curso))
+            {
+                throw new ArgumentException("O nome do curso não pode ser nulo ou vazio");
+            }
+            else
+            {
+                _cursos.Remove(curso);
+            }
+
+        }
+
+
     }
 }
